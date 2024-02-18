fca28addc3fbb9f4ee3d016a5c36f873660f3023
Maximilian Konter <maximilian.konter@protonmail.com>
2024-02-18 11:02:04 +0100

    Fix i3 colors.

--- users/_modules/i3/default.nix
+++ users/_modules/i3/default.nix
@@ -37,37 +37,64 @@ in {
       menu = "rofiWindow"; # See rofi/default.nix
 
       # Set colors according to https://github.com/catppuccin/i3
-      colors = rec {
+      colors = let
+        rosewater = "#f2d5cf";
+        flamingo = "#eebebe";
+        pink = "#f4b8e4";
+        mauve = "#ca9ee6";
+        red = "#e78284";
+        maroon = "#ea999c";
+        peach = "#ef9f76";
+        yellow = "#e5c890";
+        green = "#a6d189";
+        teal = "#81c8be";
+        sky = "#99d1db";
+        sapphire = "#85c1dc";
+        blue = "#8caaee";
+        lavender = "#babbf1";
+        text = "#c6d0f5";
+        subtext1 = "#b5bfe2";
+        subtext0 = "#a5adce";
+        overlay2 = "#949cbb";
+        overlay1 = "#838ba7";
+        overlay0 = "#737994";
+        surface2 = "#626880";
+        surface1 = "#51576d";
+        surface0 = "#414559";
+        base = "#303446";
+        mantle = "#292c3c";
+        crust = "#232634";
+      in rec {
         focused = {
-          border = "$lavender";
-          background = "$base";
-          text = "$text";
-          indicator = "$rosewater";
-          childBorder = "$lavender";
+          border = lavender;
+          background = base;
+          text = text;
+          indicator = rosewater;
+          childBorder = lavender;
         };
         unfocused = {
-          border = "$overlay0";
-          background = "$base";
-          text = "$text";
-          indicator = "$rosewater";
-          childBorder = "$overlay0";
+          border = overlay0;
+          background = base;
+          text = text;
+          indicator = rosewater;
+          childBorder = overlay0;
         };
         focusedInactive = unfocused;
         urgent = {
-          border = "$peach";
-          background = "$base";
-          text = "$peach";
-          indicator = "$overlay0";
-          childBorder = "$peach";
+          border = peach;
+          background = base;
+          text = peach;
+          indicator = overlay0;
+          childBorder = peach;
         };
         placeholder = {
-          border = "$overlay0";
-          background = "$base";
-          text = "$text";
-          indicator = "$overlay0";
-          childBorder = "$overlay0";
+          border = overlay0;
+          background = base;
+          text = text;
+          indicator = overlay0;
+          childBorder = overlay0;
         };
-        background = "$base";
+        background = base;
       };
 
       # It is super important that the left side of the binding does not include any spaces!
@@ -235,16 +262,6 @@ in {
       #  command =
       #    "command -v swww >/dev/null 2>&1 && swww init & sleep 0.5 && exec wallpaper_random || echo 'swww not installed'";
       #}];
-    };
-  };
-
-  home.file.".config/i3/colors/catppuccin-frappe" = {
-    # TODO: utilize flake and let it decide the hash
-    source = builtins.fetchurl {
-      url =
-        "https://raw.githubusercontent.com/catppuccin/i3/main/themes/catppuccin-frappe";
-      sha256 =
-        "75b0e063b0ff670173641cb5fb456052af84b78774f77c37b6c3a886fc809c51";
     };
   };
 }
