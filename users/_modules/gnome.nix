f97a75fa34149c594cae42d412269ee60b70a586
Maximilian Konter <maximilian.konter@protonmail.com>
2025-01-23 08:02:10 +0100

    Add option to unlock keyring from CLI

--- users/_modules/gnome.nix
+++ users/_modules/gnome.nix
@@ -5,10 +5,17 @@
   pkgs,
   ...
 }:
-{
-
+let
+  unlockKeyring = pkgs.writeShellScriptBin "unlock-keyring" ''
+    #!/usr/bin/env bash
+    read -rsp "Password: " pass
+    export $(echo -n "$pass" | gnome-keyring-daemon --replace --unlock)
+    unset pass
+  '';
+in {
   # GNOME related packages
   home.packages = with pkgs; [
+    unlockKeyring
     pinentry-gnome3
     gnome-tweaks
     seahorse
