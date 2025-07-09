e2bce8efa46e577fbf605849fd96c79cc93a84ae
Maximilian Konter <maximilian.konter@protonmail.com>
2025-07-09 06:08:27 +0200

    Add keybindings to vscode and set tree indent.

--- users/_modules/vscode/default.nix
+++ users/_modules/vscode/default.nix
@@ -1,3 +1,4 @@
 {...}: {
   home.file.".config/Code/User/settings.json".text = builtins.readFile ./settings.json;
+  home.file.".config/Code/User/keybindings.json".text = builtins.readFile ./keybindings.json;
 }
