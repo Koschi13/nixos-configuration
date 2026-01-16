ac3e55da13b3a48f38ca27bd684c9ab79083568a
Maximilian Konter <maximilian.konter@protonmail.com>
2026-01-16 07:44:01 +0100

    Fix gsw alias overwritten.
    Forgit added the `gsw` alias, which was messing with the oh-my-zsh git
    alias `gsw`. I added the `f` prefix to the command.
    I also sorted the commands alphabetically for easier search.

--- users/_modules/zsh/plugins/forgit.nix
+++ users/_modules/zsh/plugins/forgit.nix
@@ -13,24 +13,25 @@
     ];
     initContent = lib.mkOrder 550 ''
       # forgit alias, prefix all with f
-      forgit_log=fglo
-      forgit_diff=fgd
       forgit_add=fga
-      forgit_reset_head=fgrh
-      forgit_ignore=fgi
-      forgit_checkout_file=fgcf
-      forgit_checkout_branch=fgcb
-      forgit_branch_delete=fgbd
-      forgit_checkout_tag=fgct
-      forgit_checkout_commit=fgco
-      forgit_revert_commit=fgrc
-      forgit_clean=fgclean
-      forgit_stash_show=fgss
-      forgit_stash_push=fgsp
-      forgit_cherry_pick=fgcp
-      forgit_rebase=fgrb
       forgit_blame=fgbl
+      forgit_branch_delete=fgbd
+      forgit_checkout_branch=fgcb
+      forgit_checkout_commit=fgco
+      forgit_checkout_file=fgcf
+      forgit_checkout_tag=fgct
+      forgit_cherry_pick=fgcp
+      forgit_clean=fgclean
+      forgit_diff=fgd
       forgit_fixup=fgfu
+      forgit_switch_branch=fgsw
+      forgit_ignore=fgi
+      forgit_log=fglo
+      forgit_rebase=fgrb
+      forgit_reset_head=fgrh
+      forgit_revert_commit=fgrc
+      forgit_stash_push=fgsp
+      forgit_stash_show=fgss
     '';
   };
 }
