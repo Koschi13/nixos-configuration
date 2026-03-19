bd606156a23725930dee1f9e7ee5aa079ad934fe
Maximilian Konter <maximilian.konter@protonmail.com>
2026-03-19 13:15:31 +0100

    Further improve the SSM config
    - It's now possible to have the region configured per host to support
      multiple regions.
    - Add `sshuttle` to use it for IP range forwarding with SSM
    
    TODO: It seems like the `aws ec2 wait instance-running` is not the
          correct command to be used for waiting on an EC2 instance becoming
          ready for SSH.

--- users/_modules/ssm.nix
+++ users/_modules/ssm.nix
@@ -1,51 +1,71 @@
 {
-  lib,
+  pkgs,
   rootPath,
   ...
 }: let
   vars = import "${rootPath}/.secrets/ssm_vars.nix";
-  regionProfile = "--region ${vars.region} --profile ${vars.profile}";
-  proxyCommand = "sh -c \"aws ec2 start-instances ${regionProfile} --instance-ids %h && aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p' ${regionProfile}\"";
 
-  sshAttrs =
-    builtins.listToAttrs
-    (map (host: {
+  # list[list[{host; service;}]]
+  regions = map (config: let
+    regionProfile = "--region ${config.region} --profile ${vars.profile}";
+    startInstanceCommand = "aws ec2 start-instances ${regionProfile} --instance-ids %h";
+    waitForInstanceCommand = "aws ec2 wait instance-running ${regionProfile} --instance-ids %h";
+    startSessionCommand = "aws ssm start-session  ${regionProfile} --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'";
+    proxyCommand = "sh -c \"${startInstanceCommand} && ${waitForInstanceCommand} && ${startSessionCommand}\"";
+  in
+    map (host: {
+      # list
+      host = {
         name = host.name;
         value = {
           hostname = host.hostname;
           user = vars.user;
+          identityFile = host.identityFile;
           proxyCommand = proxyCommand;
         };
-      })
-      vars.hosts);
-  serviceListAttrs = map (host:
-    map (service: {
-      name = "${host.name}_${service.name}";
-      value = {
-        hostname = host.hostname;
-        user = vars.user;
-        identityFile = host.identityFile;
-        localForwards = [
-          {
-            bind.port = 8080;
-            host.port = service.port;
-            host.address = service.address;
-          }
-        ];
-        proxyCommand = proxyCommand;
       };
+
+      # list[list]
+      services =
+        map (service: {
+          name = "${host.name}_${service.name}";
+          value = {
+            hostname = host.hostname;
+            user = vars.user;
+            identityFile = host.identityFile;
+            localForwards = [
+              {
+                bind.port = 8080;
+                host.port = service.port;
+                host.address = service.address;
+              }
+            ];
+            proxyCommand = proxyCommand;
+          };
+        })
+        host.services;
     })
-    host.services)
-  vars.hosts;
-  serviceAttrs = builtins.listToAttrs (lib.lists.flatten serviceListAttrs);
+    config.hosts)
+  vars.regions;
+
+  hostAttrs = builtins.listToAttrs (builtins.concatLists (map (regionConfigs: map (config: config.host) regionConfigs) regions));
+  serviceAttrs = builtins.listToAttrs (builtins.concatLists (map (regionConfigs: builtins.concatLists (map (config: config.services) regionConfigs)) regions));
 in {
   programs.ssh = {
-    matchBlocks = sshAttrs // serviceAttrs;
+    matchBlocks = hostAttrs // serviceAttrs;
   };
 
-  home.sessionVariables = builtins.listToAttrs (map (host: {
-      name = host.name;
-      value = host.hostname;
-    })
-    vars.hosts);
+  home = {
+    sessionVariables = builtins.listToAttrs (builtins.concatLists (map (config:
+      map (host: {
+        name = host.name;
+        value = host.hostname;
+      })
+      config.hosts)
+    vars.regions));
+
+    packages = [
+      pkgs.sshuttle
+    ];
+  };
 }
